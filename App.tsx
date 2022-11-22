34e85b033f9ff02e17cdeb81640d1a826906aee1
Gabriel Amaro <gamaro919@gmail.com>
2022-11-21 22:38:12 -0300

    feat: added button register

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native';
 import { Appbar } from 'react-native-paper';
-import { AppbarContent } from 'react-native-paper/lib/typescript/components/Appbar/AppbarContent';
 import StatusCard from './components/status-card';
 import { chocadeiras } from './utils/data';
 
@@ -10,7 +9,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={{ width: '100%' }}>
-        <Appbar.Header style={{ backgroundColor: 'black' }}>
+        <Appbar.Header style={{ backgroundColor: 'black', alignItems: 'center' }}>
           <Appbar.BackAction
             onPress={() => { }}
             iconColor='white'
@@ -21,8 +20,7 @@ export default function App() {
                 source={require('../eclolife/utils/índice.jpeg')}
                 style={{
                   height: 50,
-                  width: 170,
-                  alignSelf: 'center',
+                  width: 150,
                 }}
               />
             }
@@ -32,14 +30,33 @@ export default function App() {
       <Text style={styles.textChocadeirasCadastradas}>CHOCADEIRAS CADASTRADAS:</Text>
 
       <FlatList
-          data={chocadeiras}
-          renderItem={({ item }: any) => (
-            <StatusCard
-              item={item}
-            />)}
+        data={chocadeiras}
+        renderItem={({ item }: any) => (
+          <StatusCard
+            item={item}
+          />)}
       />
 
-      <StatusBar style="auto" />
+      <TouchableOpacity style={{
+        backgroundColor: '#6bb155',
+        height: 50,
+        width: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 0.5,
+        borderRadius: 20,
+        margin: 10
+      }}
+        onPress={() => {
+
+        }}
+      >
+        <Text style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          color: 'white',
+        }}>CADASTRAR</Text>
+      </TouchableOpacity>
     </View>
   );
 }
