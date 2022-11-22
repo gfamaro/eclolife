6df3b1d911550b742faadcf2a5fee656b95e28b3
Gabriel Amaro <gamaro919@gmail.com>
2022-11-21 21:54:15 -0300

    feat: data

---
+++ utils/data.ts
@@ -0,0 +1,20 @@
+const chocadeiras = [
+    {
+        name: 'Chocadeira X',
+        temperatura: '30',
+        umidade: '59',
+        qntOvos: '200',
+        diasEclo: '17',
+    },
+    {
+        name: 'Chocadeira Y',
+        temperatura: '38',
+        umidade: '87',
+        qntOvos: '20',
+        diasEclo: '1',
+    },
+]
+
+export {
+    chocadeiras
+}
