b75b681d452605d8f02605610064fa504fd2543d
Gabriel Amaro <gamaro919@gmail.com>
2022-12-01 02:53:37 -0300

    feat: header bar and new screen

--- App.tsx
+++ App.tsx
@@ -5,27 +5,45 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './scr/screens/Home';
 import Register from './scr/screens/Register';
 import Doubts from './scr/screens/Doubts';
+import Logo from './components/Logo/logo';
+import { Button } from 'react-native';
+import IncubatorData from './scr/screens/IncubatorData';
 
 const Stack = createNativeStackNavigator()
 
+const screenOptionsObj: any = {
+  headerTitle: () => (<Logo width={100} />),
+  headerTitleAlign: 'center',
+  headerStyle: {
+    backgroundColor: 'black',
+  },
+  headerTintColor: '#fff'
+}
+
 export default function App() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='HomeScreen'>
-          <Stack.Screen 
-            name='HomeScreen'
-            component={Home}
-          />
-          <Stack.Screen 
-            name='Register'
-            component={Register}
-          />
-          <Stack.Screen 
-            name='Doubts'
-            component={Doubts}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={screenOptionsObj}
+        initialRouteName='Menu'>
+        <Stack.Screen
+          name='Menu'
+          component={Home}
+        />
+        <Stack.Screen
+          name='Registrar'
+          component={Register}
+        />
+        <Stack.Screen
+          name='Dúvidas'
+          component={Doubts}
+        />
+        <Stack.Screen
+          name='Dados'
+          component={IncubatorData}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
 
 
   );
