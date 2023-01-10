2666f30f9a4d14df1d782de04c44132e009e966b
Gabriel Amaro <gamaro919@gmail.com>
2023-01-10 13:20:04 -0300

    feat: data mock

--- utils/data.ts
+++ utils/data.ts
@@ -1,17 +1,31 @@
 const chocadeiras = [
     {
         name: 'Chocadeira X',
-        temperatura: '30',
-        umidade: '59',
-        qntOvos: '200',
-        diasEclo: '17',
+        temperatura: 30,
+        umidade: 59,
+        qntOvos: 20,
+        diasEclo: 1,
     },
     {
         name: 'Chocadeira Y',
-        temperatura: '38',
-        umidade: '87',
-        qntOvos: '20',
-        diasEclo: '1',
+        temperatura: 38,
+        umidade: 87,
+        qntOvos: 20,
+        diasEclo: 1
+    },
+    {
+        name: 'Chocadeira Z',
+        temperatura: 38,
+        umidade: 87,
+        qntOvos: 20,
+        diasEclo: 1
+    },
+    {
+        name: 'Chocadeira A',
+        temperatura: 38,
+        umidade: 87,
+        qntOvos: 20,
+        diasEclo: 1
     },
 ]
 
